fe150493d65b7023f2c0f15911c4a50047641af4
RolfErFloor <144017838+RolfErFloor@users.noreply.github.com>
2025-06-05 12:19:37 +0200

    update

--- AiLab8/bn_template.py
+++ AiLab8/bn_template.py
@@ -1,319 +1,92 @@
-import functools
-import random
+import itertools
 from pprint import pformat
 
-def multiply_vector_elements(vector):
-    """ return the multiplication of the vector elements """
 
-
-    return functools.reduce(mult, vector, 1)
-
-
-class Variable(object):
-    """ Node in the network. Represent a random Variable """
-
-    def __init__(self, name, assignments, probability_table, parents=[], children=[]):
+class Variable:
+    def __init__(self, name, assignments, cpt, parents=[]):
         self.name = name
-
-        # holds the possible assignments of this random variable
-        # assume certain order
-        self.assignments = {}
-        for i in range(len(assignments)):
-            self.assignments[assignments[i]] = i
-
-        # holds the distribution table of this random variable
-        for key, val in probability_table.items():
-            if len(val) != len(assignments):
-                self = None
-                raise ValueError(
-                    'data in probability table is inconsistent with possible assignments')
-        self.probability_table = probability_table
-
-        # list of dependent variables
-        self.children = children
-
-        # list of variables which this variable depends upon
+        self.assignments = assignments
+        self.assignment_idx = {val: i for i, val in enumerate(assignments)}
+        self.cpt = cpt
         self.parents = parents
 
-        # holds the marginal, pre-calculated probability to obtain each
-        # possible value
-        self.marginal_probabilities = len(assignments) * [0]
-
-        # indicates whether this node is ready to use
-        # true when the marginal probabilities were calculated
-        self.ready = False
-
-    def get_probability(self, value, parents_values):
-        return self.probability_table[parents_values][self.assignments[value]]
-
-    def get_conditional_probability(self, value, parents_values):
-        res = 0
-        given_parents_index = []
-        marginal_parents_index = []
-        for i, v in enumerate(self.parents):
-            if v.name in parents_values:
-                given_parents_index.append((i, parents_values[v.name]))
-            else:
-                marginal_parents_index.append(i)
-
-        for row_key, row_val in self.probability_table.items():
-            valid_row = 1
-
-            # check if this row should count for the marginal conditional
-            # probability
-            for gpi in given_parents_index:
-                if row_key[gpi[0]] != gpi[1]:
-                    valid_row = 0
-                    break
-
-            # if this row is valid, add the corresponding conditional
-            # probability
-            if valid_row:
-                parents_probability = 1
-                for mpi in marginal_parents_index:
-                    parents_probability *= self.parents[mpi].get_marginal_probability(row_key[mpi])
-
-                res += row_val[self.assignments[value]] * parents_probability
-        return res
-
-    def calculate_marginal_probability(self):
-        if self.ready:
-            return
-        if not self.parents:
-            for assignment, idx in self.assignments.items():
-                self.marginal_probabilities[idx] = self.probability_table[()][idx]
-        else:
-            for row_key, prob_dist in self.probability_table.items():
-                parent_prob= 1.0
-                for i, parent in enumerate(self.parents):
-                    parent_prob *= parent.get_marginal_probability(row_key[i])
-                for assignment, idx in self.assignments.items():
-                    self.marginal_probabilities[idx] += prob_dist[idx] * parent_prob
+    def get_probability(self, value, parent_values):
+        key = tuple(parent_values[p.name] for p in self.parents)
+        return self.cpt[key][self.assignment_idx[value]]
 
 
+class BayesianNetwork:
+    def __init__(self, variables):
+        self.variables = variables
+        self.var_map = {v.name: v for v in variables}
 
-        # set this Node`s state to ready
-        self.ready = True
-
-    def get_marginal_probability(self, val):
-        return self.marginal_probabilities[self.assignments[val]]
-
-
-    def add_parent(self, node):
-        """ add a parent to this Variable """
-        self.parents.append(node)
-
-    def get_children(self):
-        """ returns the children list """
-        return self.children
-
-    def is_child_of(self, node):
-        """ return boolean, indicating whether this Node is a child of a given
-            Node
-        """
-        for var in self.parents:
-            if var.name == node.name:
-                return 1
-        return 0
-
-
-class BayesianNetwork(object):
-    """ Bayesian Network implementation. This implementation incorporates few
-        assumptions (see comments).
-    """
-
-    def __init__(self):
-        """ Initialize connectivity matrix. """
-        self.variables = []     # list of variables (Nodes)
-        self.varsMap = {}       # a mapping of variable name to the actual node, for easy access
-        self.ready = False          # indication of this net state
-
-    def calculate_marginal_probabilities(self):
+    def get_joint_probability(self, assignment):
+        prob = 1.0
         for var in self.variables:
-            var.calculate_marginal_probability()
-        self.ready = True
+            parent_vals = {p.name: assignment[p.name] for p in var.parents}
+            prob *= var.get_probability(assignment[var.name], parent_vals)
+        return prob
 
-    def get_variables(self):
-        """ returns the variables """
+    def get_conditional_probability(self, query, evidence):
+        all_vars = [v.name for v in self.variables]
+        hidden_vars_numerator = [v for v in all_vars if v not in query and v not in evidence]
+        hidden_vars_denominator = [v for v in all_vars if v not in evidence]
 
-        return self.variables
+        # Numerator: P(query ∩ evidence)
+        numerator = 0.0
+        for vals in itertools.product(['false', 'true'], repeat=len(hidden_vars_numerator)):
+            hidden_assignment = dict(zip(hidden_vars_numerator, vals))
+            full = {**hidden_assignment, **query, **evidence}
+            numerator += self.get_joint_probability(full)
 
-    def get_variable(self, varName):
-        """ returns the variable with the given name """
+        # Denominator: P(evidence)
+        denominator = 0.0
+        for vals in itertools.product(['false', 'true'], repeat=len(hidden_vars_denominator)):
+            hidden_assignment = dict(zip(hidden_vars_denominator, vals))
+            full = {**hidden_assignment, **evidence}
+            denominator += self.get_joint_probability(full)
 
-        return self.varsMap[varName]
-
-    def add_variable(self, var, index=-1):  # len(variables)):
-        """ add a single Node to the net """
-
-        if index < 0:
-            self.variables.append(var)
-        else:
-            self.variables.insert(index, var)
-
-        self.varsMap[var.name] = var
-        self.ready = False  # we need to re-calculate marginals
-
-    def set_variables(self, varList):
-        self.variables = varList
-        for var in self.variables:
-            self.varsMap[var.name] = var
-        self.ready = False  # we need to re-calculate marginals
-
-    def get_joint_probability(self, values):
-        """ return the joint probability of the Nodes """
-        # COMPLETE THIS FUNCTION
-        joint_prob = 1.0
-        for var in self.variables:
-            var_val = values[var.name]
-            parent_vals = self.sub_vals(var, values)
-            joint_prob *= var.get_probability(var_val, parent_vals)
-        # Return join probability
-        return joint_prob
-
-    def get_conditional_probability(self, values, evidents):
-        """ returns the conditional probability.
-            Here I do not introduce advanced algorithms for inference (e.g. junctions trees)
-            this method implement only simple inference, namely: the joint probability of children given their parents
-            or the probability of parents given their children.
-            assumption: variables in each level are independent, or independent given their parents
-            (i.e vars in values are independent, as well as vars in evidents
-        """
-        res = 1
-
-        # when we want probability of children given their parents
-        # if self.varsMap[list(values.keys())[0]].is_child_of(self.varsMap[list(evidents.keys())[0]]):
-        if all(self.varsMap[list(values.keys())[0]].is_child_of(self.varsMap[evident]) for evident in evidents.keys()):
-            # print('probability of children given their parents')
-            for child, c_val in values.items():
-                res *= self.varsMap[child].get_conditional_probability(c_val, evidents)
-
-        # when we want probability of parents given their children
-        # make use of Bayes rule
-        # assumption: nodes in each level are independent, given their parents
-        else:
-            print('probability of parents given their children')
-
-            joint_marginal_parents = 1
-            joint_marginal_children = 1
-            joint_conditional_children = 1
-            marginal_of_evidents = 1
-
-            # calculating the joint probability of the parents
-            for parent, p_val in values.items():
-                joint_marginal_parents *= self.varsMap[parent].get_marginal_probability(p_val)
-
-            # calculating the joint probability of the children, and the joint joint probability
-            # of the children given their parents
-            for child, c_val in evidents.items():
-                joint_marginal_children *= self.varsMap[child].get_marginal_probability(c_val)
-
-                # children given their parents. here the values become the
-                # evidents!
-                joint_conditional_children *= self.varsMap[child].get_conditional_probability(c_val, values)
-
-                k = list(values.keys())[0]
-                complementary_conditional_values = values.copy()
-                complementary_conditional_values[k] = 'false' if values[k] == 'true' else 'true'
-                marginal_of_evidents = marginal_of_evidents * self.varsMap[child].get_conditional_probability(c_val, complementary_conditional_values)
-
-                # print("Child: {}".format(child))
-                # print("    Given: {}".format(complementary_conditional_values))
-
-            # uses Bayes rule, for calculating the conditional probability
-            res = (joint_conditional_children * joint_marginal_parents) / ((joint_conditional_children * joint_marginal_parents) + marginal_of_evidents * (1 - joint_marginal_parents))
-
-        return res
-
-    # helper method
-    def sub_vals(self, var, values):
-        """ return a tuple, contain all the relevant
-            assignments for the given variable (i.e - the assignments
-            pertaining to the variable`s parents."""
-        sub = []
-        for p in var.parents:
-            sub.append(values[p.name])
-        return tuple(sub)
+        return numerator / denominator if denominator > 0 else 0
 
 
-def create_random_sample(network):
-    """ creates random sample for the given network.
-        the distribution of the samples follows the joint probability function.
-        assumes binary variables. """
-    sample = {}
-    for var in network.variables:
-
-        samp = random.random()
-        assignment1 = list(var.assignments.keys())[0]
-        assignment2 = list(var.assignments.keys())[1]
-
-            sample[var.name] = assignment1
-        else:
-            sample[var.name] = assignment2
-    return sample
+def print_conditional_probability(network, query, evidence):
+    print("Given:")
+    print(pformat(evidence))
+    print("Conditional probability of:")
+    print(pformat(query))
+    result = network.get_conditional_probability(query, evidence)
+    print(f"=> Result: {result:.4f}  (≈ {round(result * 100)}%)")
 
 
-def pad(string, pad=4):
-    lines = string.split('\n')
-    padded_lines = (' ' * pad + line for line in lines)
-    return '\n'.join(padded_lines)
+def build_homework_network():
+    # CPTs
+    dt = Variable('DT', ['false', 'true'], {(): [0.7, 0.3]})
+    em = Variable('EM', ['false', 'true'], {(): [0.7, 0.3]})
+    ftl = Variable('FTL', ['false', 'true'], {(): [0.8, 0.2]})
+    v = Variable('V', ['false', 'true'], {
+        ('true',): [0.7, 0.3],
+        ('false',): [0.1, 0.9]
+    }, parents=[dt])
+    sms = Variable('SMS', ['false', 'true'], {
+        ('true', 'true'): [0.05, 0.95],
+        ('true', 'false'): [0.6, 0.4],
+        ('false', 'true'): [0.3, 0.7],
+        ('false', 'false'): [0.7, 0.3]
+    }, parents=[dt, em])
+    hc = Variable('HC', ['false', 'true'], {
+        ('true', 'true', 'true'): [0.1, 0.9],
+        ('true', 'true', 'false'): [0.2, 0.8],
+        ('true', 'false', 'true'): [0.7, 0.3],
+        ('true', 'false', 'false'): [0.8, 0.2],
+        ('false', 'true', 'true'): [0.4, 0.6],
+        ('false', 'true', 'false'): [0.5, 0.5],
+        ('false', 'false', 'true'): [0.9, 0.1],
+        ('false', 'false', 'false'): [0.99, 0.01],
+    }, parents=[dt, ftl, em])
+
+    return BayesianNetwork([dt, em, ftl, v, sms, hc])
 
 
-def print_conditional_probability(network, conditionals_vars, conditionals_evidents):
-    print('Given')
-    print(pad(pformat(conditionals_evidents)))
-    print('conditional probability of')
-    print(pad(pformat(conditionals_vars)))
-    print("is {:f}".format(
-        network.get_conditional_probability(
-            conditionals_vars,
-            conditionals_evidents
-        )))
-
-
-def print_joint_probability(network, values):
-    print('Joint probability of')
-    print(pad(pformat(values)))
-    print("is {:f}".format(network.get_joint_probability(values)))
-
-
-
-
-def sprinkler():
-    # the values kept as dictionary
-    t1 = {(): (0.5, 0.5)}
-    t2 = {('false',): (0.5, 0.5), ('true',): (0.9, 0.1)}
-    t3 = {('false',): (0.8, 0.2), ('true',): (0.2, 0.8)}
-    t4 = {
-        ('false', 'false'): (1, 0),
-        ('true', 'false'): (0.1, 0.9),
-        ('false', 'true'): (0.1, 0.9),
-        ('true', 'true'): (0.01, 0.99)
-    }
-
-    # creation of Nodes objects
-    cloudy = Variable('Cloudy', ('false', 'true'), t1)
-    sprinkler = Variable('Sprinkler', ('false', 'true'), t2, [cloudy])
-    rain = Variable('Rain', ('false', 'true'), t3, [cloudy])
-    wetgrass = Variable('WetGrass', ('false', 'true'), t4, [sprinkler, rain])
-
-    variables = [cloudy, sprinkler, rain, wetgrass]
-
-    # creation of Network
-    network = BayesianNetwork()
-    network.set_variables(variables)
-
-    # pre-calculate marginals
-    network.calculate_marginal_probabilities()
-
-    print_marginal_probabilities(network)
-
-    print('')
-
-    joint_values = {
-        'Sprinkler': 'true',
-    conditionals_evidents = {'WetGrass': 'true'}
-
-    print_conditional_probability(network, conditionals_vars, conditionals_evidents)
-    print('')
-
+if __name__ == '__main__':
+    net = build_homework_network()
+    print_conditional_probability(net, {'FTL': 'true'}, {'HC': 'true'})
